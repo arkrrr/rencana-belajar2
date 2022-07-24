eb2ba1f25c2e52dce18fab471680761a73a4d1ab
arkrrr <76622898+arkrrr@users.noreply.github.com>
2022-07-24 14:38:58 +0700

    menambahkan pekerjaan

--- jadwal-belajar.py
+++ jadwal-belajar.py
@@ -3,7 +3,7 @@ def jadwal():
   print("1. sekolah")
   print("2. eskul")
   print("3. belajar git")
-
+  print("4. ngerjain pr")
  a = input("Masukkan pelajaran kesukaan anda: ")
  if a == 'ipa':
     jadwal()
